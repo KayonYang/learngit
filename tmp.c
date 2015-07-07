b45718474ec97fe21dc0ba5c82d6f22cfecb79ce
TomYang <yangyoutao@jd.com>
2015-07-07 10:39:41 +0800

    new file

--- tmp.c
+++ tmp.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main()
-{
+{	
+	//git is a distributed version control system
+	for (int i = 0; i< 10; i++)
+	{
+	  printf("%d\n", i);
+	}
 	printf("quick learn git\n");
 	return 0;
 }
